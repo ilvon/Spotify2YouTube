ade204d4bb88490a400e67279b14e964fe24b339
thisObedience <endlesslimbo@gmail.com>
2023-10-17 03:03:51 +0800

    added some error handling

--- Spotify_to_YouTube.py
+++ Spotify_to_YouTube.py
@@ -5,6 +5,7 @@ from ytmusicapi import YTMusic
 import json
 import csv
 import os
+import sys
 
 class Spotify2Youtube():
     def __init__(self, config_file=True):
@@ -14,19 +15,30 @@ class Spotify2Youtube():
     def init_transfer(self):
         sp_playlist_title, sp_playlist_tracks = self.sp.get_playlist_tracks_info()
         ytlist_desc = input('\nPlaylist description: ')
-        ytlist_visibility = input('Playlist privacy status (PUBLIC / PRIVATE / UNLISTED): ')
+        ytlist_visibility = input('Playlist privacy status (PUBLIC / PRIVATE / UNLISTED): ').upper()
+        if ytlist_visibility not in ['PUBLIC', 'PRIVATE', 'UNLISTED']:
+            ytlist_visibility = 'PRIVATE'
+            
         print(f'\n{len(sp_playlist_tracks)} tracks will be imported from \"{sp_playlist_title}\" to YouTube')
         ytlist_id = self.yt.import_tracks(sp_playlist_title, sp_playlist_tracks,
                                           ytlist_desc, ytlist_visibility)
         print(f'\"{sp_playlist_title}\" created successfully, URL = https://www.youtube.com/playlist?list={ytlist_id}')
-        
-        os.remove('browser.json')
+        # os.remove('browser.json')
     
     class SpotifyExport():
         def __init__(self, config_file: bool):
             if config_file:
-                with open('config.json', 'r') as jsonf:
-                    cfg = json.load(jsonf)
+                self.chk_create_JSON()
+                try:
+                    with open('config.json', 'r') as jsonf:
+                        cfg = json.load(jsonf)
+                except(FileNotFoundError):
+                    print('\"config.json\" does not exist / failed to open!')
+                    sys.exit(1)
+                except Exception as err:
+                    print('An error occurred:', str(err))      
+                    sys.exit(1) 
+                           
                 self.sp_client = spotipy.Spotify(auth_manager=SpotifyOAuth(
                     client_id=cfg['spotify']['SPOTIPY_CLIENT_ID'],
                     client_secret=cfg['spotify']['SPOTIPY_CLIENT_SECRET'],
@@ -41,12 +53,40 @@ class Spotify2Youtube():
             for playlist_item in self.playlists['items']:
                 self.all_playlist_title.append(playlist_item['name'])
 
+        def chk_create_JSON(self):
+            if os.path.exists('config.json'):
+                return
+            
+            create_cfg = input('\"config.json\" does not exist. Create it? (Y/N): ')
+            if create_cfg not in ['y', 'Y']:
+                print('Script exited.')
+                sys.exit(0)
+                
+            print('Please visit https://developer.spotify.com/ to create your app and obtain WebAPI credentials.')
+            api_client_id = input('Client ID: ')
+            api_client_secret = input('Client Secret: ')
+            api_client_uri = input('Redirect URI: ')
+            SPOTIFY_CFG_TEMPLATE = {
+                'spotify':{
+                    'SPOTIPY_CLIENT_ID': api_client_id,
+                    'SPOTIPY_CLIENT_SECRET': api_client_secret,
+                    'SPOTIPY_REDIRECT_URI': api_client_uri
+                }
+            }
+            try:
+                with open('config.json', 'w') as ojson:
+                    json.dump(SPOTIFY_CFG_TEMPLATE, ojson)
+            except Exception as err:
+                print('An error occurred when creating \"config.json\": ', str(err))
+                sys.exit(1)
+
         def print_all_playlist(self):
             for i, list_name in enumerate(self.all_playlist_title):
                 print(f"{i}. {list_name}")
                 
-        def _prompt_playlist_selection(self):   # return playlist ID in list & playlist title 
-            self.print_all_playlist()
+        def _prompt_playlist_selection(self): # return playlist ID in list & playlist title 
+            print('\n') 
+            self.print_all_playlist() 
             while True:
                 index = int(input(f'Playlist to be exported (0-{len(self.all_playlist_title)-1}): ')) 
                 if (index <= len(self.all_playlist_title) - 1) and index >= 0:
@@ -121,15 +161,8 @@ class Spotify2Youtube():
         def __init__(self):     
             if not os.path.exists('browser.json'):
                 ytmusicapi.setup(filepath='browser.json')          
-            self.yt_client = YTMusic('browser.json')    
-            # self.fun_test()     
-            
-        def fun_test(self):
-            results = self.yt_client.search('I miss you, I’m sorry - Gracie Abrams - minor', filter='songs')
-            with open('gracie_songs.json', 'w', encoding='utf-8-sig') as resultjson:
-                json.dump(results, resultjson)
-                
-        
+            self.yt_client = YTMusic('browser.json')     
+                             
         # imported_tracks: list containing dict
         def import_tracks(self, new_playlist_name, imported_tracks: list, desc, privacy):
             imported_tracks_ID = []
@@ -142,36 +175,20 @@ class Spotify2Youtube():
                     print(f'{idx+1}/{total_track_no} exported.\n')
                 else:
                     print(f'{idx+1}/{total_track_no} exported.', end='\r')
-                
-            new_playlist_id = self.yt_client.create_playlist(title=new_playlist_name,
-                                                             description=desc,
-                                                             privacy_status=privacy, 
-                                                             video_ids=imported_tracks_ID)
+            try:                
+                new_playlist_id = self.yt_client.create_playlist(title=new_playlist_name,
+                                                                description=desc,
+                                                                privacy_status=privacy, 
+                                                                video_ids=imported_tracks_ID)
+            except Exception as autherr:
+                print(str(autherr) + '\nTry to delete \"browser.json\" and restart the process!')
+                del_req_header = input('Delete \"browser.json\"? (Y/N): ')
+                if del_req_header in ['Y', 'y']:
+                    os.remove('browser.json')
+                sys.exit(0)
             return new_playlist_id
 
-
-        def search_tracks(self, target_track_metadata):
-            target_track_id = ''
-            search_str = '-'.join(target_track_metadata.values())
-            results = self.yt_client.search(search_str)
-            
-            for info in results:
-                if target_track_id:
-                    break
-                if info['category'] == 'More from YouTube':
-                    continue
-                    
-                if info['category'] == 'Top result' and info['resultType'] == 'song':
-                    target_track_id = info['videoId']
-                if info['category'] == 'Songs':
-                    if target_track_metadata['title'] in info['title']:
-                        target_track_id = info['videoId']
-            if target_track_id == '':
-                # target_track_id = results[0]['videoId']
-                target_track_id = next((info['videoId'] for info in results if info['category'] == 'Songs'), None)
-            return target_track_id
-        
-        def search_tracks2(self, target_track_metadata):
+        def search_tracks(self, target_track_metadata: dict):
             search_str = '-'.join(target_track_metadata.values())
             results = self.yt_client.search(search_str)
             
@@ -182,13 +199,18 @@ class Spotify2Youtube():
                 if info['category'] == 'Top result' and info['resultType'] == 'song':
                     return info['videoId']
                 if info['category'] == 'Songs':
-                    if target_track_metadata['title'] == info['title']:
+                    if target_track_metadata['title'] in info['title']:
                         return info['videoId']
             
-            return results[0]['videoID']    # id of Top result
+            return next((info['videoId'] for info in results if info['category'] == 'Songs'), None)
                 
 
 if __name__ == '__main__':
     main_process = Spotify2Youtube(config_file=True) 
-    main_process.init_transfer()
-    # main_process.sp.export_simplified_tracks_info()
+    
+    while True:
+        main_process.init_transfer()
+        export_more = input('Continue to transfer Spotify playlist? (Y / Any key to exit): ')
+        if export_more not in ['Y', 'y']:
+            os.remove('browser.json')
+            sys.exit(0)
